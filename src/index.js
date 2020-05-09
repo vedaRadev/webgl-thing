2270907b08b085fbc5c6f766b1bb8ef3bdd67606
m-wradev <ryancarmstrong97@gmail.com>
2020-05-09 17:50:44 -0500

    buffer setup refactors, add toDrawShape function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-// TODO: install ctags on this machine
-import toInitGL, {
+import {
+	constants,
+
+	toInitGL,
+	toDrawMode,
+} from '../lib/glUtils';
+
+const {
 	GL_DATA_TYPES,
 	GL_BUFFER_TARGETS,
 	GL_BUFFER_USAGES,
-} from '../lib/glSetup';
-
+	GL_DRAW_MODES,
+} = constants;
 const { ARRAY_BUFFER } = GL_BUFFER_TARGETS;
 const { FLOAT } = GL_DATA_TYPES;
 const { STATIC_DRAW } = GL_BUFFER_USAGES;
+const { TRIANGLES } = GL_DRAW_MODES;
+
+import { object } from '../lib/fp';
+const { map, pluck } = object;
 
 const shaderIDs = ['shader-fs', 'shader-vs'];
 const bufferConfigs = [
@@ -27,23 +37,25 @@ const bufferConfigs = [
 	}
 ];
 
-const toDrawTriangle = gl => (vertexBuffer, colorBuffer) => 
+const toDrawShape = gl => 
 {
-	const shapeDataIndexInBuffer = vertexBuffer.toLength() + 1;
+	const toGLDrawMode = toDrawMode(gl);
 
-	vertexBuffer.setData([
-		0.0, 1.0, 0.0, 
-		-1.0, -1.0, 0.0, 
-		1.0, -1.0, 0.0
-	]);
+	return (generateVertices, drawMode) => 
+	{
+		const glDrawMode = toGLDrawMode(drawMode);
+		const toBufferLength = ({ data: { length }, pointLength }) => length / pointLength;
 
-	colorBuffer.setData([
-		1.0, 0.0, 0.0, 1.0, 
-		0.0, 0.0, 1.0, 1.0, 
-		0.0, 1.0, 0.0, 1.0
-	]);
+		return (...buffers) =>
+		{
+			const bufferData = buffers.map(pluck('data'));
+			const prevLength = toBufferLength(buffers[0]);
+			generateVertices(...bufferData);
+			const curLength = toBufferLength(buffers[0]);
 
-	return () => gl.drawArray(gl.TRIANGLES, shapeDataIndexInBuffer, shapeDataIndexInBuffer + 3);
+			return () => gl.drawArrays(glDrawMode, prevLength, curLength - prevLength);
+		};
+	};
 };
 
 const main = () => 
@@ -53,12 +65,35 @@ const main = () =>
 
 	const [, { vertices, colors }] = toInitGL(gl)(shaderIDs, bufferConfigs);
 
-	const drawTriangle = toDrawTriangle(gl)(vertices, colors);
-	console.log(vertices.toLength());
+	const toDrawTriangle = toDrawShape(gl)(
+		(vertexData, colorData) => 
+		{
+			vertexData.push(
+				0.0, 1.0, 0.0, 
+				-1.0, -1.0, 0.0, 
+				1.0, -1.0, 0.0
+			);
+
+			colorData.push(
+				1.0, 0.0, 0.0, 1.0, 
+				0.0, 0.0, 1.0, 1.0, 
+				0.0, 1.0, 0.0, 1.0
+			);
+		},
+		TRIANGLES
+	);
+
+	const drawTriangle = toDrawTriangle(vertices, colors);
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	gl.disable(gl.DEPTH_TEST);
+
+	gl.bindBuffer(vertices.target, vertices.buffer);
+	gl.bufferData(vertices.target, new vertices.type(vertices.data), vertices.usage);
+
+	gl.bindBuffer(colors.target, colors.buffer);
+	gl.bufferData(colors.target, new colors.type(colors.data), colors.usage);
 
 	drawTriangle();
 };
