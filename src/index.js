f4eb0fef31a1a1e48ffa6eae644050b0674c02b1
Ryan Armstrong <ryancarmstrong97@gmail.com>
2020-05-09 11:40:36 -0500

    remove comments, grab info for bufferData from returned buffers

--- src/index.js
+++ src/index.js
@@ -8,17 +8,6 @@ const { ARRAY_BUFFER } = GL_BUFFER_TARGETS;
 const { FLOAT } = GL_DATA_TYPES;
 
 const shaderIDs = ['shader-fs', 'shader-vs'];
-// const bufferConfigs = [
-// 	{
-// 		bufferID: 'foo',
-// 		bufferTarget: ARRAY_BUFFER,
-// 		bufferType: Float32Array,
-// 		attribs: [
-// 			{ name: 'position', size: 3, dataType: FLOAT },
-// 			{ name: 'color', size: 4, dataType: FLOAT }
-// 		]
-// 	},
-// ];
 const bufferConfigs = [
 	{
 		bufferID: 'vertexBuffer',
@@ -38,19 +27,17 @@ const main = () =>
 {
 	const screen = document.getElementById('screen');
 	const gl = screen.getContext('webgl2');
-	const [, buffers] = toInitGL(gl)(shaderIDs, bufferConfigs);
-
-	const { vertexBuffer, colorBuffer } = buffers;
+	const [, { vertexBuffer, colorBuffer }] = toInitGL(gl)(shaderIDs, bufferConfigs);
 	const vertices = [];
 	const colors = [];
 
 	vertices.push(0.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0);
 	gl.bindBuffer(vertexBuffer.target, vertexBuffer.buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	gl.bufferData(vertexBuffer.target, new vertexBuffer.type(vertices), gl.STATIC_DRAW);
 
 	colors.push(1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0);
 	gl.bindBuffer(colorBuffer.target, colorBuffer.buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	gl.bufferData(colorBuffer.target, new colorBuffer.type(colors), gl.STATIC_DRAW);
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
